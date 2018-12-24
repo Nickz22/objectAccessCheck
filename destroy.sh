8b5391e5cd662ab8b788aa0da229f00b4a3c09f1
Nicholas Zozaya <nzozaya@snapchat.com>
2018-12-24 11:17:12 -0800

    change devHub

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,4 @@
-heroku pipelines:destroy pipe1545604748
-heroku apps:destroy -a dev1545604748 -c dev1545604748
-heroku apps:destroy -a staging1545604748 -c staging1545604748
-heroku apps:destroy -a prod1545604748 -c prod1545604748
+heroku pipelines:destroy pipe1545677864
+heroku apps:destroy -a dev1545677864 -c dev1545677864
+heroku apps:destroy -a staging1545677864 -c staging1545677864
+heroku apps:destroy -a prod1545677864 -c prod1545677864
